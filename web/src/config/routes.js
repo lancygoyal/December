0126586fa094af820bf1ca92b05fda7bcd332cfa
Lancy Goyal <i.lancygoyal@gmail.com>
2018-01-03 23:57:47 +0530

    layouting done

--- web/src/config/routes.js
+++ web/src/config/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import SignIn from '../containers/SignIn';
 import SignUp from '../containers/SignUp';
 import App from '../containers/App';
@@ -8,26 +8,58 @@ import FormPage from '../containers/FormPage';
 import TablePage from '../containers/TablePage';
 import NotFoundPage from '../containers/NotFoundPage.js';
 
-export default store => {
-  // const state = store.getState();
-  // const requireAuth = () => {
-  //   if (!state.user.loggedIn) {
-  //     window.location.href = '/signin';
-  //   }
-  // };
+const requireAuth = store => {
+  return store.getState().user.isLoggedIn;
+};
+
+const PublicLayout = ({ component: Component }) => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/signin" component={SignIn} />
-        <Route exact path="/signup" component={SignUp} />
-        <App>
-          <Route exact path="/" component={Dashboard} />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/form" component={FormPage} />
-          <Route exact path="/table" component={TablePage} />
-        </App>
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Router>
+    <div className="UserScreens">
+      <Component />
+    </div>
+  );
+};
+
+const PrivateLayout = ({ component: Component }) => {
+  return (
+    <App>
+      <Component />
+    </App>
+  );
+};
+
+const PublicRoute = ({ component, ...rest }) => {
+  return <Route {...rest} render={props => <PublicLayout component={component} {...props} />} />;
+};
+
+const PrivateRoute = ({ component, store, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      requireAuth(store) ? (
+        <PrivateLayout component={component} {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: '/signin',
+            state: { from: props.location }
+          }}
+        />
+      )
+    }
+  />
+);
+
+export default store => {
+  return (
+    <Switch>
+      <PublicRoute exact path="/signin" component={SignIn} />
+      <PublicRoute exact path="/signup" component={SignUp} />
+      <PrivateRoute exact path="/" component={Dashboard} store={store} />
+      <PrivateRoute exact path="/dashboard" component={Dashboard} store={store} />
+      <PrivateRoute exact path="/form" component={FormPage} store={store} />
+      <PrivateRoute exact path="/table" component={TablePage} store={store} />
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 };
